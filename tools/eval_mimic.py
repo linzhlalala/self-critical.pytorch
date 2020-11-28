9cad4e78b6ad580938de74282191238a2b571a40
donghaozhang <zdhpeter1991@gmail.com>
2020-11-28 21:28:29 +1100

    work on mimic evaluation script

---
+++ tools/eval_mimic.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import json
+import numpy as np
+
+import time
+import os
+from six.moves import cPickle
+
+import captioning.utils.opts as opts
+import captioning.models as models
+from captioning.data.dataloader import *
+from captioning.data.dataloaderraw import *
+import captioning.utils.eval_utils as eval_utils
+import argparse
+import captioning.utils.misc as utils
+import captioning.modules.losses as losses
+import torch

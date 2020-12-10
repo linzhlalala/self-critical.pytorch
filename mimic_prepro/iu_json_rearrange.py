278b1c96f21507f42b5dfc047d1add202014b160
ZHIHONG LIN <zhihonglin90@gmail.com>
2020-12-10 12:52:36 +1100

    IU_ray profeat

--- mimic_prepro/iu_json_rearrange.py
+++ mimic_prepro/iu_json_rearrange.py
@@ -36,3 +36,4 @@ if __name__ == '__main__':
     anno_file = main(iu_anno_path)
 
 
+
--- scripts/prepro_feats.py
+++ scripts/prepro_feats.py
@@ -35,6 +35,10 @@ preprocess = trn.Compose([
                 trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+import sys
+from os.path import abspath, dirname, join
+sys.path.insert(0, join(abspath(dirname(__file__)),'..'))
+
 from captioning.utils.resnet_utils import myResnet
 import captioning.utils.resnet as resnet
 
@@ -61,8 +65,8 @@ def main(params):
 
     for i,img in enumerate(imgs):
         # load the image
-        # I = skimage.io.imread(os.path.join(params['images_root'], img['filepath'], img['filename']))
-        I = skimage.io.imread(os.path.join(params['images_root'], img['file_path']))
+        I = skimage.io.imread(os.path.join(params['images_root'], img['filepath'], img['filename']))
+        #I = skimage.io.imread(os.path.join(params['images_root'], img['file_path']))
         # handle grayscale input images
         if len(I.shape) == 2:
             I = I[:,:,np.newaxis]
